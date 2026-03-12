9076898ca12b357bf570276ae590fa23b4a7fd50
kahoW <35646795+wan7up@users.noreply.github.com>
2026-03-12 23:19:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ set -eu
 mkdir -p /home/node/.openclaw
 chown -R 1000:1000 /home/node/.openclaw
 
-exec gosu 1000:1000 node dist/index.js gateway --bind lan --port 18789
+export HOME=/home/node
+export NODE_ENV=production
+export TERM=xterm-256color
+export XDG_CONFIG_HOME=/home/node/.openclaw
+
+exec gosu 1000:1000 node dist/index.js gateway --allow-unconfigured --bind lan --port 18789
